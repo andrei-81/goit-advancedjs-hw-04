da9f8bf2dad231bd3ed83453bdcaa2b0f26d2061
AndriHordiichuk <166404529+AndriHordiichuk@users.noreply.github.com>
2025-01-12 18:22:24 +0200

    fixed loadMore

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import { getPicturesHtml } from "./js/render-function"
 
 
 const button = document.querySelector('.search-button');
-const inProgress = document.querySelector('.in-progress');
+const inProgress = document.querySelector('.in-progress'); //88
 const searchString = document.querySelector('.search-string')
 const gallery = document.querySelector('.gallery')
 const loadMoreButton = document.querySelector('.load-more')
@@ -35,11 +35,11 @@ const iziToastParameters = {
 button.addEventListener('click', (event) => {
     event.preventDefault()
     gallery.innerHTML = ''
-    loadMoreButton.style.display = 'none'
-    inProgress.innerHTML = `<span class="loader"></span>`;
+    setLoading()
     pageNumber = 1
     totalCount = 0
     searchStringValue = searchString.value
+
 
     findImages(searchStringValue).then((response) => {
         if (response.status != 200) {
@@ -47,9 +47,10 @@ button.addEventListener('click', (event) => {
         }
         return response
     }).then((pictures) => {
-        inProgress.innerHTML = ''
+        setLoading()
         totalCount = pictures.data.total
         if(totalCount == 0){
+            inProgress.innerHTML = ``
             iziToast.error({
                 message: "Sorry, there are no images matching your search query. Please try again!",
                 ...iziToastParameters, 
@@ -58,7 +59,7 @@ button.addEventListener('click', (event) => {
             const picturesList = pictures.data.hits
             const picturesHtml = getPicturesHtml(picturesList)
             gallery.innerHTML = picturesHtml
-            loadMoreButton.style.display = 'block'
+            showLoadMore()
             lightbox.refresh()
         }
     })
@@ -67,49 +68,69 @@ button.addEventListener('click', (event) => {
         message: error,
         ...iziToastParameters,
     })
-    console.log(error)})
+    console.log(error)
+    showLoadMore()
+})
     
 })
 
+function setLoading() {
+    loadMoreButton.style.display = 'none'
+    inProgress.innerHTML = `<span class="loader"></span>`
+}
+
+function showLoadMore() {
+    inProgress.innerHTML = ``;
+    loadMoreButton.style.display = 'block'
+}
+
 
 loadMoreButton.addEventListener('click', (event) => {
+    
     event.preventDefault()
+    setLoading()
     if(totalCount <= pageNumber * PICTURES_ON_PAGE) {
         iziToast.error({
             message: "We're sorry, but you've reached the end of search results.",
             ...iziToastParameters, 
         })
-        loadMoreButton.style.display = 'none'
+        inProgress.innerHTML = ``
         return
     } 
-    
-    pageNumber++
+    else {
+        pageNumber++
 
-    findImages(searchStringValue, pageNumber).then((response) => {
-        if (response.status != 200) {
-            throw new Error(response.status);
-        }
-        return response
-    }).then((pictures) => {
-        inProgress.innerHTML = ''
-        const picturesList = pictures.data.hits
-        const picturesHtml = getPicturesHtml(picturesList)
-        gallery.insertAdjacentHTML('beforeend', picturesHtml)
-        lightbox.refresh()
-        const galleryItemElement = document.querySelector('.gallery-item')
-        const galleryItem = galleryItemElement.getBoundingClientRect()
-        const computedStyle = window.getComputedStyle(gallery);
-        const gap = parseFloat(computedStyle.gap || computedStyle.rowGap || 0)
-        window.scrollBy({
-            top: (galleryItem.height + gap) * 2,
-            behavior: "smooth",
-          })
-    })
-    .catch((error) => {iziToast.error({
-        title: "Something bad happened",
-        message: error,
-        ...iziToastParameters,
-    })
-    console.log(error)})
+        findImages(searchStringValue, pageNumber)
+        .then((response) => {
+            if (response.status != 200) {
+                throw new Error(response.status);
+            }
+            return response})
+        .then((pictures) => {
+            const picturesList = pictures.data.hits
+            const picturesHtml = getPicturesHtml(picturesList)
+            gallery.insertAdjacentHTML('beforeend', picturesHtml)
+            lightbox.refresh()
+            const galleryItemElement = document.querySelector('.gallery-item')
+            const galleryItem = galleryItemElement.getBoundingClientRect()
+            const computedStyle = window.getComputedStyle(gallery);
+            const gap = parseFloat(computedStyle.gap || computedStyle.rowGap || 0)
+            window.scrollBy({
+                top: (galleryItem.height + gap) * 2,
+                behavior: "smooth",
+            })
+            showLoadMore()
+        })
+        .catch((error) => {
+            iziToast.error({
+                title: "Something bad happened",
+                message: error,
+                ...iziToastParameters,})  
+            console.log(error)
+            showLoadMore()
+        })
+
+    }
+
 })
 
